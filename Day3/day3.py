f720da6d7b70a994234f9c4a81ee997db1509efe
ttx170 <tom@twmo.co.uk>
2021-12-03 16:52:49 +0000

    Removing static things

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,7 +1,7 @@
 with open (r"C:\Users\tom\Downloads\input3.txt") as file:
     inputs = file.read().splitlines()
-zeros=[0,0,0,0,0,0,0,0,0,0,0,0]
-ones=[0,0,0,0,0,0,0,0,0,0,0,0]
+zeros=[0] * len(inputs[0])
+ones=[0] * len(inputs[0])
 gamma=''
 epsilon=''
 for i in inputs:
@@ -28,8 +28,10 @@ print(epsilonint)
 print(total)
 
 #part2
-o2list=[inputs,[],[],[],[],[],[],[],[],[],[],[],[]]
-co2list=[inputs,[],[],[],[],[],[],[],[],[],[],[],[]]
+o2list=[[] for i in range(len(inputs[0])+1)]
+co2list=[[] for i in range(len(inputs[0])+1)]
+o2list[0]=inputs
+co2list[0]=inputs
 for x in range(12):
     o2one=0
     o2zero=0
@@ -43,7 +45,8 @@ for x in range(12):
     if o2one >= o2zero: keep = '1' 
     else: keep='0'
     for i in o2list[x]:
-        if keep == i[x]:
+        if len(o2list[x]) == 1: o2list[x+1].append(i) 
+        elif keep == i[x]:
             o2list[x+1].append(i)   
     for i in co2list[x]:
         if i[x] == '1':
@@ -52,14 +55,12 @@ for x in range(12):
             co2zero+=1
     if co2zero <= co2one: keep = '0' 
     else: keep='1'
-    for i in co2list[x]: 
-        if keep == i[x]:
+    for i in co2list[x]:
+        if len(co2list[x]) == 1: co2list[x+1].append(i) 
+        elif keep == i[x]:
             co2list[x+1].append(i)
-
-print(o2list[12])
-print(co2list[9])
 o2int = int(o2list[12][0],2)
-co2int = int(co2list[9][0],2)
+co2int = int(co2list[12][0],2)
 print(co2int)
 print(o2int)
 p2total = co2int*o2int
